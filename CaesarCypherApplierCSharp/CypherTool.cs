df47981504a828dab7056dfe6ae93e0c35e0708c
Aaron <a.rcottrill521@gmail.com>
2018-08-01 15:17:33 -0400

    Build a successful caesar cypher encoder and decoder.

---
+++ CaesarCypherApplierCSharp/CypherTool.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaesarCypherApplierCSharp
+{
+    public static class CypherTool
+    {
+        /**
+         * Takes a word one character at time,
+         * then unscrambles the cypher. 
+         * 
+         * @param keyCodes : The phrase to be restored
+         * @param rawAscii : if the phrase in ascii format, defaults to true
+         * @param shift : the shift for the cypher, defaults to 0
+         * @param separater : the seperator for the text, defaults to a double quotation mark.
+         */
+        public static String DecodeCypher(String keyCodes,bool rawAscii = true, int shift = 0, char separater = '\"')
+        {
+            StringBuilder builder = new StringBuilder(); 
+            String[] keys = keyCodes.Split(separater);
+            foreach(String key in keys)
+            {
+                if(rawAscii)
+                {
+                    bool parsed = int.TryParse(key, out int number);
+                    if(parsed)
+                    {
+                        number -= shift;
+                        builder.Append((char)number);
+                    }
+                }
+                else
+                {
+                    char value = key.ToCharArray()[0];
+                    int number = value - shift;
+                    builder.Append((char)number);
+                }
+               
+               
+            }
+            return builder.ToString(); 
+        }
+
+
+        /**
+         * Takes a word one character at time,
+         * then shifts it. 
+         * 
+         * @param keyCodes : The phrase to be encoded
+         * @param rawAscii : if the phrase in ascii format, defaults to true
+         * @param shift : the shift for the cypher, defaults to 0
+         * @param separater : the seperator for the text, defaults to a double quotation mark.
+         */
+        public static String EncodeCypher(String keyCodes, bool rawAscii = true, int shift = 0, char separater = '\"')
+        {
+            StringBuilder builder = new StringBuilder();
+            String[] keys = keyCodes.Split(separater);
+            foreach (String key in keys)
+            {
+                if (rawAscii)
+                {
+                    bool parsed = int.TryParse(key, out int number);
+                    if (parsed)
+                    {
+                        number += shift;
+                        builder.Append((char)number);
+                    }
+                }
+                else
+                {
+                    char value = key.ToCharArray()[0];
+                    int number = value + shift;
+                    builder.Append((char)number);
+                }
+
+
+            }
+            return builder.ToString();
+        }
+    }
+}
--- CaesarCypherApplierCSharp/Program.cs
+++ CaesarCypherApplierCSharp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 
 namespace CaesarCypherApplierCSharp
 {
@@ -6,7 +6,11 @@ namespace CaesarCypherApplierCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String text = CypherTool.DecodeCypher(keyCodes:"53,53,58,59,55,57,84,77,52,77,", rawAscii:true,shift:15,separater:',');
+            Console.Out.WriteLine(text);
+            Console.Write("Press <Enter> to exit... ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+
         }
     }
 }
